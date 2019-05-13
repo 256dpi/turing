cc2a1251aba7e7def6bdf0df1325b8376e7ca0c2
Joël Gähwiler <joel.gaehwiler@gmail.com>
2019-05-13 19:20:50 +0200

    added lock instructions

---
+++ stdset/lock/acquire.go
@@ -0,0 +1,93 @@
+package lock
+
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/256dpi/turing"
+)
+
+type Lock struct {
+	Value string    `json:"value"`
+	Time  time.Time `json:"time"`
+}
+
+type Acquire struct {
+	Key     []byte    `json:"key"`
+	Value   string    `json:"value"`
+	Time    time.Time `json:"time"`
+	Timeout time.Time `json:"timeout"`
+	Locked  bool      `json:"locked"`
+}
+
+func (a *Acquire) Name() string {
+	return "stdset/lock.Acquire"
+}
+
+func (a *Acquire) Build() turing.Instruction {
+	return &Acquire{}
+}
+
+func (a *Acquire) Encode() ([]byte, error) {
+	return json.Marshal(a)
+}
+
+func (a *Acquire) Decode(data []byte) error {
+	return json.Unmarshal(data, a)
+}
+
+func (a *Acquire) Execute(txn *turing.Transaction) error {
+	// reset fields
+	a.Locked = false
+
+	// prepare lock
+	var lock Lock
+
+	// get pair
+	pair, err := txn.Get(a.Key)
+	if err != nil {
+		return err
+	}
+
+	// check content if missing
+	if pair != nil {
+		// decode lock
+		err = pair.LoadValue(func(value []byte) error {
+			_ = json.Unmarshal(value, &lock)
+			return nil
+		})
+		if err != nil {
+			return err
+		}
+
+		// cancel if lock is still active and has another value
+		if lock.Time.Before(a.Time) && lock.Value != a.Value {
+			return nil
+		}
+	}
+
+	// configure lock
+	lock.Value = a.Value
+	lock.Time = a.Time
+
+	// encode lock
+	bytes, err := json.Marshal(lock)
+	if err != nil {
+		return err
+	}
+
+	// set new lock
+	err = txn.Set(a.Key, bytes)
+	if err != nil {
+		return err
+	}
+
+	// set flag
+	a.Locked = true
+
+	return nil
+}
+
+func (a *Acquire) Cardinality() int {
+	return 1
+}
---
+++ stdset/lock/release.go
@@ -0,0 +1,77 @@
+package lock
+
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/256dpi/turing"
+)
+
+type Release struct {
+	Key      []byte    `json:"key"`
+	Value    string    `json:"value"`
+	Time     time.Time `json:"time"`
+	Unlocked bool      `json:"unlocked"`
+}
+
+func (r *Release) Name() string {
+	return "stdset/lock.Release"
+}
+
+func (r *Release) Build() turing.Instruction {
+	return &Release{}
+}
+
+func (r *Release) Encode() ([]byte, error) {
+	return json.Marshal(r)
+}
+
+func (r *Release) Decode(data []byte) error {
+	return json.Unmarshal(data, r)
+}
+
+func (r *Release) Execute(txn *turing.Transaction) error {
+	// reset fields
+	r.Unlocked = false
+
+	// prepare lock
+	var lock Lock
+
+	// get pair
+	pair, err := txn.Get(r.Key)
+	if err != nil {
+		return err
+	}
+
+	// check content if missing
+	if pair != nil {
+		// decode lock
+		err = pair.LoadValue(func(value []byte) error {
+			_ = json.Unmarshal(value, &lock)
+			return nil
+		})
+		if err != nil {
+			return err
+		}
+
+		// cancel if lock is still active and has another value
+		if lock.Time.Before(r.Time) && lock.Value != r.Value {
+			return nil
+		}
+
+		// delete lock
+		err = txn.Delete(r.Key)
+		if err != nil {
+			return err
+		}
+
+		// set flag
+		r.Unlocked = true
+	}
+
+	return nil
+}
+
+func (r *Release) Cardinality() int {
+	return 1
+}
