21a08f1e39bfb0f7a4230df11810ee5674f6ee49
Joël Gähwiler <joel.gaehwiler@gmail.com>
2020-04-06 23:50:48 +0300

    simplify

--- examples/counter/main.go
+++ examples/counter/main.go
@@ -105,6 +105,17 @@ func main() {
 	}
 }
 
+type observer struct{}
+
+func (*observer) Init() {
+	fmt.Printf("==> Init\n")
+}
+
+func (*observer) Process(i turing.Instruction) bool {
+	fmt.Printf("==> %+v\n", i)
+	return true
+}
+
 func printer(machine *turing.Machine) {
 	for {
 		// wait some time
