58b698f1a99976f44e5e8e45173a9539ad3c4d8f
Joël Gähwiler <joel.gaehwiler@gmail.com>
2020-04-07 14:23:43 +0300

    added benchmarks

--- value_test.go
+++ value_test.go
@@ -9,12 +9,12 @@ import (
 func TestFullValue(t *testing.T) {
 	in := Value{
 		Kind:  FullValue,
-		Value: []byte("-foo"),
+		Value: []byte("foo"),
 	}
 
 	bytes, err := EncodeValue(in)
 	assert.NoError(t, err)
-	assert.Equal(t, []byte("f-foo"), bytes)
+	assert.NotEmpty(t, bytes)
 
 	out, err := DecodeValue(bytes)
 	assert.NoError(t, err)
@@ -27,10 +27,14 @@ func TestStackValue(t *testing.T) {
 		Stack: []Operand{
 			{
 				Name:  "foo",
-				Value: []byte("bar"),
+				Value: []byte("foo"),
 			},
 			{
 				Name:  "bar",
+				Value: []byte("bar"),
+			},
+			{
+				Name:  "baz",
 				Value: []byte("baz"),
 			},
 		},
@@ -43,4 +47,79 @@ func TestStackValue(t *testing.T) {
 	out, err := DecodeValue(bytes)
 	assert.NoError(t, err)
 	assert.Equal(t, in, out)
+}
+
+func BenchmarkEncodeFullValue(b *testing.B) {
+	value := Value{
+		Kind:  FullValue,
+		Value: []byte("foo"),
+	}
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		_, err := EncodeValue(value)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func BenchmarkEncodeStackValue(b *testing.B) {
+	value := Value{
+		Kind: StackValue,
+		Stack: []Operand{
+			{
+				Name:  "foo",
+				Value: []byte("foo"),
+			},
+			{
+				Name:  "bar",
+				Value: []byte("bar"),
+			},
+			{
+				Name:  "baz",
+				Value: []byte("baz"),
+			},
+		},
+	}
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		_, err := EncodeValue(value)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func BenchmarkDecodeFullValue(b *testing.B) {
+	data := []byte("\x01foo")
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		_, err := DecodeValue(data)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func BenchmarkDecodeStackValue(b *testing.B) {
+	data := []byte("\x02\x03\x03foo\x03foo\x03bar\x03bar\x03baz\x03baz")
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		_, err := DecodeValue(data)
+		if err != nil {
+			panic(err)
+		}
+	}
 }
