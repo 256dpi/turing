3754a430d40d43ff33b263e800c11b34989e5844
Joël Gähwiler <joel.gaehwiler@gmail.com>
2020-04-08 17:20:12 +0300

    renamed

--- merger.go
+++ merger.go
@@ -7,14 +7,14 @@ import (
 
 type merger struct {
 	registry *registry
-	values   [][]byte
+	stack    [][]byte
 	order    bool
 }
 
 var mergerPool = sync.Pool{
 	New: func() interface{} {
 		return &merger{
-			values: make([][]byte, 0, 100),
+			stack: make([][]byte, 0, 100),
 		}
 	},
 }
@@ -23,28 +23,28 @@ func newMerger(registry *registry, value []byte) *merger {
 	// borrow merger
 	merger := mergerPool.Get().(*merger)
 	merger.registry = registry
-	merger.values = append(merger.values, value)
+	merger.stack = append(merger.stack, value)
 	merger.order = true
 
 	return merger
 }
 
 func (m *merger) MergeNewer(value []byte) error {
-	// sort values
-	m.sort(true)
+	// sort stack
+	m.sortStack(true)
 
 	// add value
-	m.values = append(m.values, value)
+	m.stack = append(m.stack, value)
 
 	return nil
 }
 
 func (m *merger) MergeOlder(value []byte) error {
-	// sort values
-	m.sort(false)
+	// sort stack
+	m.sortStack(false)
 
 	// add value
-	m.values = append(m.values, value)
+	m.stack = append(m.stack, value)
 
 	return nil
 }
@@ -53,19 +53,19 @@ func (m *merger) Finish() ([]byte, error) {
 	// return merger
 	defer func() {
 		m.registry = nil
-		for i := range m.values {
-			m.values[i] = nil
+		for i := range m.stack {
+			m.stack[i] = nil
 		}
-		m.values = m.values[:0]
+		m.stack = m.stack[:0]
 		mergerPool.Put(m)
 	}()
 
-	// sort values
-	m.sort(true)
+	// sort stack
+	m.sortStack(true)
 
 	// decode values
-	values := make([]Value, 0, len(m.values))
-	for _, op := range m.values {
+	values := make([]Value, 0, len(m.stack))
+	for _, op := range m.stack {
 		value, err := DecodeValue(op)
 		if err != nil {
 			return nil, err
@@ -108,16 +108,16 @@ func (m *merger) Finish() ([]byte, error) {
 	}
 }
 
-func (m *merger) sort(fwd bool) {
+func (m *merger) sortStack(fwd bool) {
 	// check if already sorted
 	if m.order == fwd {
 		return
 	}
 
-	// reverse values
-	for i := 0; i < len(m.values)/2; i++ {
-		j := len(m.values) - 1 - i
-		m.values[i], m.values[j] = m.values[j], m.values[i]
+	// reverse stack
+	for i := 0; i < len(m.stack)/2; i++ {
+		j := len(m.stack) - 1 - i
+		m.stack[i], m.stack[j] = m.stack[j], m.stack[i]
 	}
 
 	// set order
